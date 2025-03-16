12a54dd23f64e8834225ec89e6809b511e928f03
Rinka <kujourinka@gmail.com>
2025-03-17 01:01:26 +0900

    update lock food skill v2

--- lock_food_skill_v2.lua
+++ lock_food_skill_v2.lua
@@ -13,8 +13,11 @@ app.PlayerManager
 --]]
 
 local meal_flag = false
+local meal_effect_call_from_script = false
 
-local log_prefix = "LOCK_FOOD_SKILL_V2"
+local on_start_check = false
+
+local log_prefix = "LOCK_FOOD_V2"
 
 local meal_effect_data = sdk.create_instance("app.cMealEffect", true):add_ref()
 if meal_effect_data == nil then return end
@@ -26,22 +29,48 @@ local function get_hunter_status()
     return hunterStatus
 end
 
-local function save_current_meal()
+local function save_current_meal(time)
     local mealEffect = get_hunter_status()._MealEffect._MealEffect
+    if not on_start_check then
+        local mem_time_duration = mealEffect._DurationTimer
+        if mem_time_duration % 1 ~= 0 or mem_time_duration % 10 ~= 0 then
+            local file_time_duration = tonumber(fs.read("lock_food_skill.dat"))
+            mealEffect:set_field("_DurationTimer", file_time_duration)
+        end
+    end
     -- parameters must be specified due to multiple overloaded functions
     meal_effect_data:call("copyFrom(app.cMealEffect)", mealEffect)
+
+    -- todo: record get_hunter_status()._MealEffect._MealEffect._DurationTimer to file
+    if time ~= nil then
+        meal_effect_data:set_field("_DurationTimer", tonumber(time))
+    else
+        fs.write("lock_food_skill.dat", mealEffect._DurationTimer)
+    end
     meal_flag = true
 end
 
 local function do_meal()
+    meal_effect_call_from_script = true
     get_hunter_status()._MealEffect:setMealEffect(meal_effect_data, true)
+    meal_effect_call_from_script = false
 end
 
 -- hook quest start
 local quest_start = sdk.find_type_definition("app.cQuestPlaying"):get_method("enter")
 local quest_end = sdk.find_type_definition("app.cQuestPlaying"):get_method("exit")
 
+
 local function pre_quest_start(args)
+    local meal_effect = get_hunter_status()._MealEffect
+    if (not on_start_check) and meal_effect._IsEffectActive then
+        -- todo: read from file and write to get_hunter_status()._MealEffect._MealEffect._DurationTimer
+        local duration_timer = fs.read("lock_food_skill.dat")
+        if duration_timer ~= "" then
+            save_current_meal(duration_timer)
+        end
+        on_start_check = true
+    end
     if meal_flag then
         do_meal()
     end
@@ -60,7 +89,9 @@ local function pre_meal_effect(args)
 end
 
 local function post_meal_effect(retval)
-    save_current_meal()
+    if not meal_effect_call_from_script then
+        save_current_meal()
+    end
     return retval
 end
 
